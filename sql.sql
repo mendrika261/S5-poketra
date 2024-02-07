132e0e6cd2b141abf75fb3f7d1efbaa577068b70
Restyled.io <commits@restyled.io>
2024-02-07 13:52:38 +0000

    Restyled by pg_format

--- sql.sql
+++ sql.sql
@@ -1,89 +1,186 @@
-create view v_format as
-select f.id id_format, m.nom nom_modele, f."nomFormat" nom_format, s.id id_style, s.nom nom_style, f."idModele" id_modele
-from "Format" f
-         join "Modele" m on f."idModele" = m.id
-         join "Style" s on m."idStyle" = s.id;
+CREATE VIEW v_format AS
+SELECT
+    f.id id_format,
+    m.nom nom_modele,
+    f."nomFormat" nom_format,
+    s.id id_style,
+    s.nom nom_style,
+    f."idModele" id_modele
+FROM
+    "Format" f
+    JOIN "Modele" m ON f."idModele" = m.id
+    JOIN "Style" s ON m."idStyle" = s.id;
 
+CREATE VIEW style_mp AS
+SELECT
+    "idStyle" id_style,
+    "idMatierePremiere" id_mp,
+    nom nom_mp
+FROM
+    "MPStyle"
+    JOIN "MatierePremiere" mp ON "idMatierePremiere" = mp.id;
 
-create view style_mp as select "idStyle" id_style, "idMatierePremiere" id_mp, nom nom_mp from "MPStyle" JOIN "MatierePremiere" mp on "idMatierePremiere" = mp.id;
+CREATE VIEW v_liste AS
+SELECT
+    m.id idModele,
+    f.id idFormat,
+    mp.id idMatierePremiere,
+    mp.quantite quantite
+FROM
+    "Modele" m
+    JOIN "Format" f ON m."idStyle" = f."idModele"
+    JOIN "MpModele" mp ON f.id = mp."idFormat";
 
-create view v_liste as select m.id idModele, f.id idFormat,  mp.id idMatierePremiere, mp.quantite quantite
-                       from "Modele" m JOIN "Format" f on m."idStyle" = f."idModele" JOIN "MpModele" mp on f.id = mp."idFormat";
+CREATE VIEW v_modele_mp AS
+SELECT
+    "MpModele".id,
+    "MpModele"."idFormat",
+    "MpModele"."idMatierePremiere",
+    mp.nom mp_nom,
+    "MpModele".quantite,
+    "MpModele".quantite * mp.prix AS prix,
+    v_format.id_format,
+    v_format.nom_format,
+    v_format.id_modele,
+    v_format.nom_modele,
+    v_format.id_style,
+    v_format.nom_style
+FROM
+    "MpModele"
+    JOIN v_format ON "MpModele"."idFormat" = v_format.id_format
+    JOIN "MatierePremiere" mp ON "MpModele"."idMatierePremiere" = mp.id;
 
-create view v_modele_mp as
-SELECT "MpModele".id,
-       "MpModele"."idFormat",
-       "MpModele"."idMatierePremiere",
-       mp.nom mp_nom,
-       "MpModele".quantite,
-       "MpModele".quantite * mp.prix as prix,
-       v_format.id_format,
-       v_format.nom_format,
-       v_format.id_modele,
-       v_format.nom_modele,
-       v_format.id_style,
-       v_format.nom_style
-FROM "MpModele"
-         JOIN v_format ON "MpModele"."idFormat" = v_format.id_format
-         JOIN "MatierePremiere" mp on "MpModele"."idMatierePremiere" = mp.id;
+CREATE VIEW v_prix_format AS
+SELECT
+    id_format,
+    sum(prix) AS prix,
+    nom_format,
+    nom_modele,
+    nom_style
+FROM
+    v_modele_mp
+GROUP BY
+    "id_format",
+    "nom_format",
+    "nom_modele",
+    "nom_style";
 
-create view v_prix_format as
-select id_format, sum(prix) as prix, nom_format, nom_modele, nom_style from v_modele_mp
-group by "id_format", "nom_format", "nom_modele", "nom_style";
+SELECT
+    *
+FROM
+    v_prix_format
+WHERE
+    prix BETWEEN 30000 AND 50000;
 
+CREATE VIEW v_stock_entree AS
+SELECT
+    *
+FROM
+    "MouvementStock"
+WHERE
+    quantite > 0;
 
+CREATE VIEW v_stock_sortie AS
+SELECT
+    *
+FROM
+    "MouvementStock"
+WHERE
+    quantite < 0;
 
-select * from v_prix_format where prix between 30000 and 50000;
+CREATE VIEW v_stock AS
+SELECT
+    "idMatierePremiere" AS id,
+    mp.nom,
+    sum(quantite)
+FROM
+    "MouvementStock"
+    JOIN "MatierePremiere" mp ON "MouvementStock"."idMatierePremiere" = mp.id
+GROUP BY
+    "idMatierePremiere",
+    mp.nom;
 
-create view v_stock_entree as
-select * from "MouvementStock" WHERE quantite > 0;
-
-create view v_stock_sortie as
-select * from "MouvementStock" WHERE quantite < 0;
-
-create view v_stock as
-    select "idMatierePremiere" as id, mp.nom, sum(quantite) from "MouvementStock"
-                                              join "MatierePremiere" mp on "MouvementStock"."idMatierePremiere" = mp.id
-                                              group by "idMatierePremiere", mp.nom;
-
-
-
-select * from v_service_prix;
+SELECT
+    *
+FROM
+    v_service_prix;
 
 CREATE OR REPLACE VIEW v_service_prix AS
-SELECT "idModele", sum("Service"."prixHoraire" * "ServiceParProduit".heure * "ServiceParProduit".personnel) as cout
-FROM "Service" JOIN "ServiceParProduit"
-                    ON "Service".id="ServiceParProduit"."idService" GROUP BY "idModele";
+SELECT
+    "idModele",
+    sum("Service"."prixHoraire" * "ServiceParProduit".heure * "ServiceParProduit".personnel) AS cout
+FROM
+    "Service"
+    JOIN "ServiceParProduit" ON "Service".id = "ServiceParProduit"."idService"
+GROUP BY
+    "idModele";
 
 CREATE OR REPLACE VIEW v_cout_fabrication_produit AS
-SELECT id as id_format, "Format".coefficient, cout as cout_base, cout * "Format".coefficient as cout_total
-FROM "Format" JOIN v_service_prix
-                   ON "Format"."idModele"="v_service_prix"."idModele";
+SELECT
+    id AS id_format,
+    "Format".coefficient,
+    cout AS cout_base,
+    cout * "Format".coefficient AS cout_total
+FROM
+    "Format"
+    JOIN v_service_prix ON "Format"."idModele" = "v_service_prix"."idModele";
 
 CREATE OR REPLACE VIEW v_cout_mp_produit AS
-SELECT id_format, SUM(prix) from v_modele_mp group by id_format;
+SELECT
+    id_format,
+    SUM(prix)
+FROM
+    v_modele_mp
+GROUP BY
+    id_format;
 
 CREATE OR REPLACE VIEW v_cout_produit AS
-SELECT v_cout_mp_produit.id_format, cout_total + sum as cout_total,
-       prixvente, prixvente - (cout_total + sum) as marge, "nomFormat" as nom_format,
-       "Modele"."nom" as nom_modele, "Style"."nom" as nom_style
-FROM v_cout_fabrication_produit JOIN v_cout_mp_produit
-                                     ON v_cout_fabrication_produit.id_format=v_cout_mp_produit.id_format
-                                JOIN "Format" ON "Format".id=v_cout_mp_produit.id_format
-                                JOIN "Modele" ON "Modele".id="Format"."idModele"
-                                JOIN "Style" ON "Style".id="Modele"."idStyle";
+SELECT
+    v_cout_mp_produit.id_format,
+    cout_total + sum AS cout_total,
+    prixvente,
+    prixvente - (cout_total + sum) AS marge,
+    "nomFormat" AS nom_format,
+    "Modele"."nom" AS nom_modele,
+    "Style"."nom" AS nom_style
+FROM
+    v_cout_fabrication_produit
+    JOIN v_cout_mp_produit ON v_cout_fabrication_produit.id_format = v_cout_mp_produit.id_format
+    JOIN "Format" ON "Format".id = v_cout_mp_produit.id_format
+    JOIN "Modele" ON "Modele".id = "Format"."idModele"
+    JOIN "Style" ON "Style".id = "Modele"."idStyle";
 
 CREATE VIEW "v_info_personnel" AS
-SELECT "idPersonnel" as id_personnel, "nom", "nomService" as nom_service, s."prixHoraire" as prix_horaire_base,
-       floor(extract(day from (now()-"dateAffectation"))/365) as anciennete, "dateAffectation" date_affectation
-from "Affectation" a
-JOIN "Personnel" p ON "idPersonnel" = p.id
-JOIN "Service" s ON s.id = a.idservice
-order by "dateAffectation";
+SELECT
+    "idPersonnel" AS id_personnel,
+    "nom",
+    "nomService" AS nom_service,
+    s."prixHoraire" AS prix_horaire_base,
+    floor(extract(day FROM (now() - "dateAffectation")) / 365) AS anciennete,
+    "dateAffectation" date_affectation
+FROM
+    "Affectation" a
+    JOIN "Personnel" p ON "idPersonnel" = p.id
+    JOIN "Service" s ON s.id = a.idservice
+ORDER BY
+    "dateAffectation";
 
-SELECT * FROM "v_info_personnel";
+SELECT
+    *
+FROM
+    "v_info_personnel";
 
-SELECT nom as nom_poste, coefficient as coeff_poste, id id_poste
-FROM "Poste"
-JOIN (SELECT min(abs(5-anciennete)) as poste_anciennete
-from "Poste" p order by poste_anciennete limit 1) as min_poste on anciennete = 5-poste_anciennete
+SELECT
+    nom AS nom_poste,
+    coefficient AS coeff_poste,
+    id id_poste
+FROM
+    "Poste"
+    JOIN (
+        SELECT
+            min(abs(5 - anciennete)) AS poste_anciennete
+        FROM
+            "Poste" p
+        ORDER BY
+            poste_anciennete
+        LIMIT 1) AS min_poste ON anciennete = 5 - poste_anciennete
